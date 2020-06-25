cce8f9812577c425fb1b2d7d6b2699acdd39f484
vikyathshirva <vicky.fames@gmail.com>
2020-06-26 00:12:41 +0530

    First App

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Vikyath Shirva on 25/06/20.
+//  Copyright © 2020 Vikyath Shirva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+} 
+
